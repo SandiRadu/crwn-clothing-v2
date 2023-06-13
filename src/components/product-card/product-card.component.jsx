b816c717e2023cfa93b95cf5e00a8ad5d4b58e6d
sandica.radu <sandica.g.radu@gmail.com>
2023-06-13 18:28:00 +0300

    fixind the problems from cart

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
-
 import Button from '../button/button.component';
 
 import './product-card.styles.scss';
@@ -19,8 +18,8 @@ export const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType='inverted' onClick={addProductToCart}>
-        Add to card
+      <Button buttonType="inverted" onClick={() => addProductToCart(product)}>
+        Add to cart
       </Button>
     </div>
   );
